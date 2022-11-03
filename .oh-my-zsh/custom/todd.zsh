b2937fa8367766bf3f8482da0f463b4a084bac2b
Todd Santaniello <loopyzort@gmail.com>
2022-11-02 20:40:45 -0700

    Update todd.zsh
    Just updating to include my custom git functions and remove aliases, as I'm going to try using the git plugin more

--- .oh-my-zsh/custom/todd.zsh
+++ .oh-my-zsh/custom/todd.zsh
@@ -13,21 +13,28 @@ export ANDROID_HOME=/Users/toddsantaniello/Library/Android/sdk
 export PATH=$JRE_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
 
 alias ls='ls -alF'
-alias ga='git add'
-alias gl='git log'
-alias gs='git status'
-alias gd='git diff'
-alias gdc='git diff --cached'
-alias gm='git commit -m'
-alias gma='git commit -am'
-alias gb='git branch'
-alias gc='git checkout'
-alias gra='git remote add'
-alias grr='git remote rm'
-alias gp='git pull --rebase'
-alias gcl='git clone'
-alias gist='gist -pc'
-alias grw='./gradlew'
 alias devdeb='./gradlew installDevDeb'
+alias gsquash='_gsquash'
+alias gbdlocals='_gbdlocals'
+alias gitrmlocal="!f() { git branch | egrep -v \"(^\*|master|dev)\" | egrep $1"
+
 # Set PATH, MANPATH, etc., for Homebrew.
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+_gsquash() {
+  if [ "$1" != "" ]
+  then
+    git rebase -i HEAD~$1
+  else
+    echo "add how many commits you want to squash"
+  fi
+}
+
+_gbdlocals() {
+  if [ "$1" != "" ]
+  then
+    git branch | egrep -v "(^\*|master|dev)" | egrep $1 | command xargs -n 1 git branch -D
+  else
+    echo "add a starting string for deletion"
+  fi
+}
